6849011446152ac400904ab6ab62c083ed102b2e
dm4 <sunrisedm4@gmail.com>
2012-04-27 01:38:48 +0800

    create tools/ dir

--- analyzeAPK.sh
+++ analyzeAPK.sh
@@ -1,4 +1,4 @@
-#!/bin/sh 
+#!/bin/sh
 
 [ ! -n "$1" ] && exit 0
 
@@ -7,12 +7,13 @@
 #####
 APK_LOCATION=$1;
 DIR_NAME=`echo $APK_LOCATION | sed -En 's/\.apk$//p'`
+SOOT_CLASSPATH="framework/core/classes_dex2jar.jar:framework/bouncycastle/classes_dex2jar.jar:framework/ext/classes_dex2jar.jar:framework/framework/classes_dex2jar.jar:framework/android.policy/classes_dex2jar.jar:framework/services/classes_dex2jar.jar:framework/core-junit/classes_dex2jar.jar:framework/com.htc.commonctrl/classes_dex2jar.jar:framework/com.htc.framework/classes_dex2jar.jar:framework/com.htc.android.pimlib/classes_dex2jar.jar:framework/com.htc.android.easopen/classes_dex2jar.jar:framework/com.scalado.util.ScaladoUtil/classes_dex2jar.jar:framework/com.orange.authentication.simcard/classes_dex2jar.jar:framework/android.supl/classes_dex2jar.jar:framework/kafdex/classes_dex2jar.jar:$DIR_NAME/classes_dex2jar.jar"
 
 #####
 # unzip apk file
 #####
 echo "[0;32m=====> unzip apk file[0m"
-rm -rf $DIR_NAME
+[ -d $DIR_NAME ] && rm -rf $DIR_NAME
 mkdir -p $DIR_NAME
 unzip $APK_LOCATION -d $DIR_NAME
 
@@ -20,19 +21,19 @@ unzip $APK_LOCATION -d $DIR_NAME
 # Translate AndroidManifest file from binary to human readable
 #####
 echo "[0;32m=====> Get AndroidManifest[0m"
-java -jar AXMLPrinter2.jar $DIR_NAME/AndroidManifest.xml > $DIR_NAME/AndroidManifest-real.xml
+java -jar tools/AXMLPrinter2.jar $DIR_NAME/AndroidManifest.xml > $DIR_NAME/AndroidManifest-real.xml
 
 #####
 # dex 2 jar
 # Retrieve the classes from dex file
 #####
 echo "[0;32m=====> dex2jar[0m"
-./dex2jar-0.0.9.8/dex2jar.sh $DIR_NAME/classes.dex || (echo "[0;31m=====> Classes.dex not exist [0m" && exit)
+tools/dex2jar-0.0.9.8/dex2jar.sh $DIR_NAME/classes.dex || (echo "[0;31m=====> Classes.dex not exist [0m" && exit)
 (cd $DIR_NAME && jar xvf classes_dex2jar.jar)
 
 #####
 # Use soot.jar to generate dot file for constructing CFG
 #####
 echo "[0;32m=====> soot[0m"
-java -jar soot-2.5.0.jar -dump-cfg jb.uce -cp /System/Library/Frameworks/JavaVM.framework/Classes/classes.jar:../lib:$DIR_NAME -process-path $DIR_NAME -d $DIR_NAME/sootOutput
+java -jar tools/soot-2.5.0.jar -dump-cfg jb.uce -cp $SOOT_CLASSPATH -process-path $DIR_NAME -d $DIR_NAME/sootOutput
 
